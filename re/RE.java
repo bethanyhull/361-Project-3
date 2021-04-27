fe5a064822a36076cded42905d963164f0c27272
Bethany Hull <bethanyhull@u.boisestate.edu>
2021-04-27 16:34:06 -0600

    stuff

--- re/RE.java
+++ re/RE.java
@@ -2,6 +2,11 @@ package re;
 
 import fa.nfa.NFA;
 
+/**
+ * @author bethanyhull
+ *
+ */
+
 public class RE implements REInterface {
 	private String regEx;
 
@@ -10,22 +15,31 @@ public class RE implements REInterface {
 	}
 
 	public NFA parse () {
+		NFA nfa = new NFA();
+	
 		//TODO: flesh out this method
 		return null;
 
 	}
-
+	
+	
 	/* Recursive descent parsing internals. */
 
 	/**
+	 * returns the next item of input without consuming it;
 	 * 
-	 * 
-	 * @return 
+	 * @return next char in the regex string
 	 */
 	private char peek() {
 		return regEx.charAt(0);
 	}
 
+	
+	/**
+	 * consumes the next item of input, failing if not equal to c.
+	 * 
+	 * @param c - char to be removed from the front of the regex string
+	 */
 	private void eat(char c) {
 		if (peek() == c)
 			this.regEx = this.regEx.substring(1) ;
@@ -33,22 +47,48 @@ public class RE implements REInterface {
 			throw new 
 			RuntimeException("Expected: " + c + "; got: " + peek()) ;
 	}  
-
+	
+	
+	/**
+	 * returns the next item of input and consumes it
+	 * 
+	 * @return next char in the regex string
+	 */
 	private char next() {
 		char c = peek() ;
 		eat(c) ;
 		return c ;
 	}
 
+	/**
+	 * Checks if more input is available
+	 * 
+	 * @return - true if there are more then 0 chars left in regex string
+	 */
 	private boolean more() {
 		return regEx.length() > 0 ;
 	}
 
 
+	
+	
 	/* Regular expression term types. */
 
 	private NFA regex() {
-		return null;
+		NFA term = term() ;
+
+	    if (more() && peek() == '|') {
+	      eat ('|') ;
+	      NFA regex = regex();
+	      
+	      // TODO: need a method to handle this
+	      //Creeate a new NFA that combines the two in an either/or configuration
+	      //return new Choice(term,regex) 
+	      return null;
+	    } else {
+	     return term ;
+	    }
+		
 		//TODO: flesh out this method
 	}
 
@@ -60,7 +100,7 @@ public class RE implements REInterface {
 	private NFA factor() {
 		NFA baseNFA = base();
 		//TODO: Snippet from assignmnt
-		// More ccode goes here
+		// More code goes here
 
 		return baseNFA;
 	}
