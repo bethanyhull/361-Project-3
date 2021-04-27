978af3a5234a21123a8f4a379206c4b4fffa4d0b
Bethany Hull <bethanyhull@u.boisestate.edu>
2021-04-27 09:13:53 -0600

    first commit

---
+++ re/RE.java
@@ -0,0 +1,19 @@
+package re;
+
+import fa.nfa.NFA;
+
+public class RE implements REInterface {
+
+	public RE(String regEx) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public NFA getNFA() {
+		// TODO
+		// Break up the string into manageable sections using http://matt.might.net/articles/parsing-regex-with-recursive-descent/
+		return null;
+	}
+	
+
+}
