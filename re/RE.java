d3f25702276151206e63f56150cade1cda4a7ecf
Bethany Hull <bethanyhull@u.boisestate.edu>
2021-04-27 10:46:39 -0600

    added started code from the example given

--- re/RE.java
+++ re/RE.java
@@ -3,17 +3,80 @@ package re;
 import fa.nfa.NFA;
 
 public class RE implements REInterface {
+	private String regEx;
 
 	public RE(String regEx) {
-		// TODO Auto-generated constructor stub
+		this.regEx = regEx;
+	}
+
+	public NFA parse () {
+		//TODO: flesh out this method
+		return null;
+
+	}
+
+	/* Recursive descent parsing internals. */
+
+	/**
+	 * 
+	 * 
+	 * @return 
+	 */
+	private char peek() {
+		return regEx.charAt(0);
+	}
+
+	private void eat(char c) {
+		if (peek() == c)
+			this.regEx = this.regEx.substring(1) ;
+		else
+			throw new 
+			RuntimeException("Expected: " + c + "; got: " + peek()) ;
+	}  
+
+	private char next() {
+		char c = peek() ;
+		eat(c) ;
+		return c ;
+	}
+
+	private boolean more() {
+		return regEx.length() > 0 ;
+	}
+
+
+	/* Regular expression term types. */
+
+	private NFA regex() {
+		return null;
+		//TODO: flesh out this method
+	}
+
+	private NFA term() {
+		return null;
+		//TODO: flesh out this method
+	}
+
+	private NFA factor() {
+		NFA baseNFA = base();
+		//TODO: Snippet from assignmnt
+		// More ccode goes here
+
+		return baseNFA;
+	}
+
+	private NFA base() {
+		return null;
+		//TODO: flesh out this method
 	}
 
 	@Override
 	public NFA getNFA() {
 		// TODO
-		// Break up the string into manageable sections using http://matt.might.net/articles/parsing-regex-with-recursive-descent/
+		// Break up the string into manageable sections using http://matt.might.net/articles/parsing-NFA-with-recursive-descent/
 		return null;
 	}
+	
 	
 
 }
